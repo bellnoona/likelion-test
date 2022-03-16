10bba1a027b3fc0d1f7328532f61706e7db7bbde
Soohyeon Kim <tngusglaso@gmail.com>
2022-03-16 23:16:58 +0900

    HTML, JS: Refactoring (shorten code length)

--- KSH_LION.js
+++ KSH_LION.js
@@ -3,11 +3,6 @@
 const loginCheckBtn = document.querySelector('.login__check-btn');
 const loginCheckImg = document.querySelector('#login__check-img');
 
-loginCheckBtn.addEventListener('click', (e) => {
-  const type = e.target.dataset.type;
-  if (type === 'empty') {
-    loginCheckImg.classList.remove('invisible');
-  } else {
-    loginCheckImg.classList.add('invisible');
-  }
+loginCheckBtn.addEventListener('click', () => {
+  loginCheckImg.classList.toggle('invisible');
 });
