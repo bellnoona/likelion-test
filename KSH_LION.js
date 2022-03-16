19b1c1803693e7e66411b52726955bbba6ba1e4c
Soohyeon Kim <tngusglaso@gmail.com>
2022-03-16 22:54:32 +0900

    JS: Handling login button

--- KSH_LION.js
+++ KSH_LION.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const loginCheckBtn = document.querySelector('.login__check-btn');
+const loginCheckImg = document.querySelector('#login__check-img');
+
+loginCheckBtn.addEventListener('click', (e) => {
+  const type = e.target.dataset.type;
+  if (type === 'empty') {
+    loginCheckImg.classList.remove('invisible');
+  } else {
+    loginCheckImg.classList.add('invisible');
+  }
+});
